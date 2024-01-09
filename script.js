36b52d5a426502110c389df85706d8196bc11acd
ASHISH KUMAR PANDIT <118014206+Itspandit30@users.noreply.github.com>
2024-01-09 11:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// JavaScript function that prints its own name
+function functionName() {
+    alert("functionName");
+}
+
+// Another example function
+function AccioJob() {
+    alert("AccioJob");
+}
+
+// Calling the functions
+functionName();
+AccioJob();
